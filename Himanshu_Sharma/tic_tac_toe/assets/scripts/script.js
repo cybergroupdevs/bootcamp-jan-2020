e2170a83eba92cdd0afce685726b9d5ab212021f
= <=>
2020-01-18 18:32:17 +0530

    Refactored Code and fixed minor bugs

--- Himanshu_Sharma/tic_tac_toe/assets/scripts/script.js
+++ Himanshu_Sharma/tic_tac_toe/assets/scripts/script.js
@@ -20,27 +20,17 @@ var turn = false;
 
 
 (function setUpEventListeners(){
-    var player1 = 'X';
-    var player2 = 'O';
-
-    document.querySelector('.player-1').classList.add('current-player');
+    this.init();
 
     document.querySelector('.btn--restart').addEventListener('click', init);
 
     document.addEventListener('click', (e)=>{
         const clickedClass = `${e.toElement.classList[1]}`;
-        console.log(clickedClass);
 
         if(clickedClass && blocks[clickedClass] === 0){
             //Adding event listener to each block
-            document.querySelector('.player-1').classList.toggle('current-player');
-            document.querySelector('.player-2').classList.toggle('current-player');
-           
-
-            document.querySelector(`.${clickedClass}`).textContent = ((turn === false)? player1 : player2);
-            blocks[`${clickedClass}`] = ((turn === false)? 1 : -1);
-            turn = !turn;
-            console.log(clickedClass);
+            this.togglePlayer(); 
+            this.manageTurnAndBlocksObj();
 
             //Finding the winner
             if(this.findWinner()){
@@ -53,6 +43,20 @@ var turn = false;
         }
     });
 })();
+
+togglePlayer = () => {
+    document.querySelector('.player-1').classList.toggle('current-player');
+    document.querySelector('.player-2').classList.toggle('current-player');
+}
+
+manageTurnAndBlocksObj = () => {
+    var player1 = 'X';
+    var player2 = 'O';
+
+    document.querySelector(`.${clickedClass}`).textContent = ((turn === false)? player1 : player2);
+    blocks[`${clickedClass}`] = ((turn === false)? 1 : -1);
+    turn = !turn;
+}
 
 checkForNoResult = () => {
     let countZeroes = 0;
@@ -79,49 +83,49 @@ findWinner = () => {
         switch(-3){
             case blocks[`block-00`]+blocks[`block-01`]+blocks[`block-02`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 1rem; width: 85%; position: absolute; top: 14%; left: 7%; transition: all .5s");
+                    "style", "background-color: black; height: 1rem; width: 85%; position: absolute; top: 14%; left: 7%; transition: all .2s");
                 return 'Player2';
             }
             
             case blocks[`block-10`]+blocks[`block-11`]+blocks[`block-12`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 1rem; width: 85%; position: absolute; top: 47%; left: 7%; transition: all .5s");
+                    "style", "background-color: black; height: 1rem; width: 85%; position: absolute; top: 47%; left: 7%; transition: all .2s");
                 return 'Player2';
             }
             
             case blocks[`block-20`]+blocks[`block-21`]+blocks[`block-22`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 1rem; width: 85%; position: absolute; top: 80%; left: 7%; transition: all .5s");
+                    "style", "background-color: black; height: 1rem; width: 85%; position: absolute; top: 80%; left: 7%; transition: all .2s");
                 return 'Player2';
             }
             
             case blocks[`block-00`]+blocks[`block-10`]+blocks[`block-20`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 85%; width: 1rem; position: absolute; top: 8%; left: 16%; transition: all .5s");
+                    "style", "background-color: black; height: 85%; width: 1rem; position: absolute; top: 8%; left: 16%; transition: all .2s");
                 return 'Player2';
             }
             
             case blocks[`block-01`]+blocks[`block-11`]+blocks[`block-21`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 85%; width: 1rem; position: absolute; top: 8%; left: 49%; transition: all .5s");
+                    "style", "background-color: black; height: 85%; width: 1rem; position: absolute; top: 8%; left: 49%; transition: all .2s");
                 return 'Player2';
             }
             
             case blocks[`block-02`]+blocks[`block-12`]+blocks[`block-22`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 85%; width: 1rem; position: absolute; top: 8%; left: 82%; transition: all .5s");
+                    "style", "background-color: black; height: 85%; width: 1rem; position: absolute; top: 8%; left: 82%; transition: all .2s");
                 return 'Player2';
             }
             
             case blocks[`block-00`]+blocks[`block-11`]+blocks[`block-22`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 108%; width: 1rem; position: absolute; top: -5%; left: 49%; transition: all .5s; transform: rotate(-45deg)");
+                    "style", "background-color: black; height: 108%; width: 1rem; position: absolute; top: -5%; left: 49%; transition: all .2s; transform: rotate(-45deg)");
                 return 'Player2';
             }
             
             case blocks[`block-02`]+blocks[`block-11`]+blocks[`block-20`]: case blocks[`block-00`]+blocks[`block-11`]+blocks[`block-22`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 108%; width: 1rem; position: absolute; top: -5%; left: 49%; transition: all .5s; transform: rotate(45deg)");
+                    "style", "background-color: black; height: 108%; width: 1rem; position: absolute; top: -5%; left: 49%; transition: all .2s; transform: rotate(45deg)");
                 return 'Player2';
             }
             
@@ -130,53 +134,61 @@ findWinner = () => {
         switch(3){
             case blocks[`block-00`]+blocks[`block-01`]+blocks[`block-02`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 1rem; width: 85%; position: absolute; top: 14%; left: 7%; transition: all .5s");
+                    "style", "background-color: black; height: 1rem; width: 85%; position: absolute; top: 14%; left: 7%; transition: all .2s");
                 return 'Player1';
             }
             
             case blocks[`block-10`]+blocks[`block-11`]+blocks[`block-12`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 1rem; width: 85%; position: absolute; top: 47%; left: 7%; transition: all .5s");
+                    "style", "background-color: black; height: 1rem; width: 85%; position: absolute; top: 47%; left: 7%; transition: all .2s");
                 return 'Player1';
             }
             
             case blocks[`block-20`]+blocks[`block-21`]+blocks[`block-22`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 1rem; width: 85%; position: absolute; top: 80%; left: 7%; transition: all .5s");
+                    "style", "background-color: black; height: 1rem; width: 85%; position: absolute; top: 80%; left: 7%; transition: all .2s");
                 return 'Player1';
             }
 
             case blocks[`block-00`]+blocks[`block-10`]+blocks[`block-20`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 85%; width: 1rem; position: absolute; top: 8%; left: 16%; transition: all .5s");
+                    "style", "background-color: black; height: 85%; width: 1rem; position: absolute; top: 8%; left: 16%; transition: all .2s");
                 return 'Player1';
             }
             
             case blocks[`block-01`]+blocks[`block-11`]+blocks[`block-21`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 85%; width: 1rem; position: absolute; top: 8%; left: 49%; transition: all .5s");
+                    "style", "background-color: black; height: 85%; width: 1rem; position: absolute; top: 8%; left: 49%; transition: all .2s");
                 return 'Player1';
             }
             
             case blocks[`block-02`]+blocks[`block-12`]+blocks[`block-22`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 85%; width: 1rem; position: absolute; top: 8%; left: 82%; transition: all .5s");
+                    "style", "background-color: black; height: 85%; width: 1rem; position: absolute; top: 8%; left: 82%; transition: all .2s");
                 return 'Player1';
             }
             
             case blocks[`block-00`]+blocks[`block-11`]+blocks[`block-22`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 108%; width: 1rem; position: absolute; top: -5%; left: 49%; transition: all .5s; transform: rotate(-45deg)");
+                    "style", "background-color: black; height: 108%; width: 1rem; position: absolute; top: -5%; left: 49%; transition: all .2s; transform: rotate(-45deg)");
                 return 'Player1';
             }
             
             case blocks[`block-02`]+blocks[`block-11`]+blocks[`block-20`]: case blocks[`block-00`]+blocks[`block-11`]+blocks[`block-22`]: {
                 document.querySelector('.strike_through').setAttribute(
-                    "style", "background-color: black; height: 108%; width: 1rem; position: absolute; top: -5%; left: 49%; transition: all .5s; transform: rotate(45deg)");
+                    "style", "background-color: black; height: 108%; width: 1rem; position: absolute; top: -5%; left: 49%; transition: all .2s; transform: rotate(45deg)");
                 return 'Player1';
             }    
         }
     }
+}
+
+manageNonClicking = () => {
+        let allBlocks = document.querySelectorAll('.block');
+        
+        allBlocks.forEach(function(block) {
+        block.style.zIndex = -1;
+    });
 }
 
 printWinner = (winner) => {
@@ -184,11 +196,7 @@ printWinner = (winner) => {
         document.querySelector('.msg').textContent = `No Result!!! Press Reset to play again...`;   
     }else{
         document.querySelector('.msg').textContent = `${winner} is the winner!!! Press Reset to play again...`;
-        let allBlocks = document.querySelectorAll('.block');
-    
-        allBlocks.forEach(function(block) {
-        block.style.zIndex = -1;
-    });
+        this.manageNonClicking();
     }
     // setTimeout(()=>{
     //     document.querySelector('.msg').textContent = ``;
@@ -197,7 +205,10 @@ printWinner = (winner) => {
 
 function init(){
     turn = false;
+    document.querySelector('.player-1').classList.add('current-player');
+    document.querySelector('.player-2').classList.remove('current-player');
 
+    //Resetting Value of GlobalBlocksObj
     for(let row = 0; row < 3; row++){
         for(let column = 0; column < 3; column++){
             document.querySelector(`.block-${row}${column}`).textContent = '';
@@ -207,6 +218,7 @@ function init(){
 
     let allBlocks = document.querySelectorAll('.block');
     
+    //Manage of the ZIndex of blocks back to normal,-> This has to be done because of Remove Listeners Overhead
     allBlocks.forEach(function(block) {
         block.style.zIndex = 0;
     });
