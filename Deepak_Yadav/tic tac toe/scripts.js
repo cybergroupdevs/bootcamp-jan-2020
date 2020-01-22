1ee184b34a1db6b99bf679868e660a3ed7bd556b
Deepakyadav19 <dy30152@gmail.com>
2020-01-22 18:01:01 +0530

    scripts updated

--- Deepak_Yadav/tic tac toe/scripts.js
+++ Deepak_Yadav/tic tac toe/scripts.js
@@ -1,6 +1,6 @@
 var options = "X";
 //background color will change once any perfect combination occurs
-function highLight(cell1, cell2, cell3, symbol){
+function highLight(cell1, cell2, cell3){
     btnDisable();
     document.getElementById(cell1).style.backgroundColor = 'red';
     document.getElementById(cell2).style.backgroundColor = 'red';
@@ -9,46 +9,16 @@ function highLight(cell1, cell2, cell3, symbol){
 
 //game will start from starting with a new different background color
 function reset(){
-    var b1 = document.getElementById("b1");
-    var b2 = document.getElementById("b2");
-    var b3 = document.getElementById("b3");
-    var b4 = document.getElementById("b4");
-    var b5 = document.getElementById("b5");
-    var b6 = document.getElementById("b6");
-    var b7 = document.getElementById("b7");
-    var b8 = document.getElementById("b8");
-    var b9 = document.getElementById("b9");
-//initializing buttons with # value
-    b1.innerHTML = "#";
-    b2.innerHTML = "#";
-    b3.innerHTML = "#";
-    b4.innerHTML = "#";
-    b5.innerHTML = "#";
-    b6.innerHTML = "#";
-    b7.innerHTML = "#";
-    b8.innerHTML = "#";
-    b9.innerHTML = "#";
-
-    b1.style.backgroundColor = "#99780b";
-    b2.style.backgroundColor = "#99780b";
-    b3.style.backgroundColor = "#99780b";
-    b4.style.backgroundColor = "#99780b";
-    b5.style.backgroundColor = "#99780b";
-    b6.style.backgroundColor = "#99780b";
-    b7.style.backgroundColor = "#99780b";
-    b8.style.backgroundColor = "#99780b";
-    b9.style.backgroundColor = "#99780b";
-
-    b1.disabled = false;
-    b2.disabled = false;
-    b3.disabled = false;
-    b4.disabled = false;
-    b5.disabled = false;
-    b6.disabled = false;
-    b7.disabled = false;
-    b8.disabled = false;
-    b9.disabled = false;
+    for(let i=1;i<=9;i++)
+    {
+         var b = document.getElementById("b"+i);
+         b.innerHTML = "#";
+         b.style.backgroundColor = "#99780b";
+         b.disabled=false;
+    }
 }
+   
+
 
 //Winning combination
 function checker(){
@@ -96,65 +66,15 @@ function checker(){
 
 //The button pressed will be disabled and symbol 'X' or 'O' will appear on it
 function setButton(button, options){
-    if(button == 1){
-        var btn = document.getElementById("b1");
-        btn.innerHTML = options;
-        btn.disabled = true;
+    
+    for(let i=1; i<=9; i++){
+        if(button === i){
+            var btn = document.getElementById("b"+i);
+            btn.innerHTML = options;
+            btn.disabled = true;
+        }
     }
-    else if(button == 2){
-        var btn = document.getElementById("b2");
-        btn.innerHTML = options;
-        btn.disabled = true;
-    }
-    else if(button == 3){
-        var btn = document.getElementById("b3");
-        btn.innerHTML = options;
-        btn.disabled = true;
-    }
-    else if(button == 4){
-        var btn = document.getElementById("b4");
-        btn.innerHTML = options;
-        btn.disabled = true;
-    }
-    else if(button == 5){
-        var btn = document.getElementById("b5");
-        btn.innerHTML = options;
-        btn.disabled = true;
-    }
-    else if(button == 6){
-        var btn = document.getElementById("b6");
-        btn.innerHTML = options;
-        btn.disabled = true;
-    }
-    else if(button == 7){
-        var btn = document.getElementById("b7");
-        btn.innerHTML = options;
-        btn.disabled = true;
-    }
-    else if(button == 8){
-        var btn = document.getElementById("b8");
-        btn.innerHTML = options;
-        btn.disabled = true;
-    }
-    else if(button == 9){
-        var btn = document.getElementById("b9");
-        btn.innerHTML = options;
-        btn.disabled = true;
-    }
-    //Checks if a WINNING pattern is made
     checker();
-}
-
-//for alternate inputs 
-function clicker(button){
-    if(options == "X"){
-        options = "O";
-        setButton(button, options);
-    }
-    else if(options == "O"){
-        options = "X";
-        setButton(button, options);
-    }
 }
 
 function btnDisable(){
