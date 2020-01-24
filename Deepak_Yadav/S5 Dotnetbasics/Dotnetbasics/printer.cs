b6edd8a0462998a8accc5989887dab53d157e116
Deepakyadav19 <dy30152@gmail.com>
2020-01-24 13:47:59 +0530

    dotnet crud assignment submitted

---
+++ Deepak_Yadav/S5 Dotnetbasics/Dotnetbasics/printer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dotnetbasics
+{
+    class printer
+    {
+        public void printMessage(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
