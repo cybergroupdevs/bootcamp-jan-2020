2942e3f77aba5fb4481bcdef9d59bdc51a8fae94
horaanchal <horaanchal@gmail.com>
2020-01-24 15:32:25 +0530

    Calculator issues resolved

--- Anchal_Hora/Calc/assets/scripts/script.js
+++ Anchal_Hora/Calc/assets/scripts/script.js
@@ -18,11 +18,4 @@ document.calc.display.value= text.replace(text[len-1], "")
 
 }
 
-function insertOperator(op){
-    txt = document.getElementById("display").value;
-    l = txt.length;
 
-    if ((txt.charAt(l-1) != "+") && (txt.charAt(l-1) != "-") && (txt.charAt(l-1) != "X") && (txt.charAt(l-1) != "/")) {
-        document.getElementById("display").value = txt+ String(op)
-    }
-}
