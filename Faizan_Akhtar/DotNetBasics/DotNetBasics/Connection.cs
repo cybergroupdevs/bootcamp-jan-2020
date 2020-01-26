4c42b21cf913ecb7dcae77452723aa2fd9c6f924
akhtarfzn <38faizan@gmail.com>
2020-01-26 18:33:50 +0530

    adding C sharp project 1 - CRUD operations

---
+++ Faizan_Akhtar/DotNetBasics/DotNetBasics/Connection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DotNetBasics
+{
+    class Connection
+    {
+       public static SqlConnection doConnection()
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.ConnectionString = "Data Source=CYG290\\SQLEXPRESS;Initial Catalog=cSharpBasic;Integrated Security=True;";
+
+                SqlConnection connection = new SqlConnection(builder.ConnectionString);
+                
+                    return connection;
+                
+
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+    }
+}
