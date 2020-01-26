1d049563ac8094ae4904c01d62f7ce2cbb21bd6e
akhtarfzn <38faizan@gmail.com>
2020-01-26 18:26:25 +0530

    making changes in tic tac toe game as suggested by nikhil

--- Faizan_Akhtar/project1-tic-tac-toe/assets/scripts/tic-tac-toe.js
+++ Faizan_Akhtar/project1-tic-tac-toe/assets/scripts/tic-tac-toe.js
@@ -10,69 +10,23 @@ function clicker(button){
         setButton(button, gameSymbol);
     }
 }
-
 function btnDisable(){
-    document.getElementById("btn1").disabled = true;
-    document.getElementById("btn2").disabled = true;
-    document.getElementById("btn3").disabled = true;
-    document.getElementById("btn4").disabled = true;
-    document.getElementById("btn5").disabled = true;
-    document.getElementById("btn6").disabled = true;
-    document.getElementById("btn7").disabled = true;
-    document.getElementById("btn8").disabled = true;
-    document.getElementById("btn9").disabled = true;
+    for(var i=1; i<=9; i++){
+        document.getElementById("btn"+i).disabled = true;
+    }
 }
-
 function setButton(button, gameSymbol){
-    if(button == 1){
-        var btn = document.getElementById("btn1");
-        btn.innerHTML = gameSymbol;
-        btn.disabled = true;
-    }
-    else if(button == 2){
-        var btn = document.getElementById("btn2");
-        btn.innerHTML = gameSymbol;
-        btn.disabled = true;
-    }
-    else if(button == 3){
-        var btn = document.getElementById("btn3");
-        btn.innerHTML = gameSymbol;
-        btn.disabled = true;
-    }
-    else if(button == 4){
-        var btn = document.getElementById("btn4");
-        btn.innerHTML = gameSymbol;
-        btn.disabled = true;
-    }
-    else if(button == 5){
-        var btn = document.getElementById("btn5");
-        btn.innerHTML = gameSymbol;
-        btn.disabled = true;
-    }
-    else if(button == 6){
-        var btn = document.getElementById("btn6");
-        btn.innerHTML = gameSymbol;
-        btn.disabled = true;
-    }
-    else if(button == 7){
-        var btn = document.getElementById("btn7");
-        btn.innerHTML = gameSymbol;
-        btn.disabled = true;
-    }
-    else if(button == 8){
-        var btn = document.getElementById("btn8");
-        btn.innerHTML = gameSymbol;
-        btn.disabled = true;
-    }
-    else if(button == 9){
-        var btn = document.getElementById("btn9");
-        btn.innerHTML = gameSymbol;
-        btn.disabled = true;
-    }
+            var btn = document.getElementById("btn"+button);
+            btn.innerHTML = gameSymbol;
+            btn.disabled = true;
     checker();
 }
-
-
+function gameFinished(box1, box2, box3){
+    btnDisable();
+    document.getElementById(box1).style.backgroundColor = '#ff4757';
+    document.getElementById(box2).style.backgroundColor = '#ff4757';
+    document.getElementById(box3).style.backgroundColor = '#ff4757';
+}
 function checker(){
     var b1 = document.getElementById("btn1").innerHTML;
     var b2 = document.getElementById("btn2").innerHTML;
@@ -110,53 +64,11 @@ function checker(){
         gameFinished("btn7", "btn8", "btn9");
     }
 }
-
-function gameFinished(box1, box2, box3){
-    btnDisable();
-    document.getElementById(box1).style.backgroundColor = '#ff4757';
-    document.getElementById(box2).style.backgroundColor = '#ff4757';
-    document.getElementById(box3).style.backgroundColor = '#ff4757';
-    alert("Game Finished");
-}
-
 function resetBtn(){
-    var b1 = document.getElementById("btn1");
-    var b2 = document.getElementById("btn2");
-    var b3 = document.getElementById("btn3");
-    var b4 = document.getElementById("btn4");
-    var b5 = document.getElementById("btn5");
-    var b6 = document.getElementById("btn6");
-    var b7 = document.getElementById("btn7");
-    var b8 = document.getElementById("btn8");
-    var b9 = document.getElementById("btn9");
-
-    b1.innerHTML = "";
-    b2.innerHTML = "";
-    b3.innerHTML = "";
-    b4.innerHTML = "";
-    b5.innerHTML = "";
-    b6.innerHTML = "";
-    b7.innerHTML = "";
-    b8.innerHTML = "";
-    b9.innerHTML = "";
-
-    b1.style.backgroundColor = "#5352ed";
-    b2.style.backgroundColor = "#5352ed";
-    b3.style.backgroundColor = "#5352ed";
-    b4.style.backgroundColor = "#5352ed";
-    b5.style.backgroundColor = "#5352ed";
-    b6.style.backgroundColor = "#5352ed";
-    b7.style.backgroundColor = "#5352ed";
-    b8.style.backgroundColor = "#5352ed";
-    b9.style.backgroundColor = "#5352ed";
-
-    b1.disabled = false;
-    b2.disabled = false;
-    b3.disabled = false;
-    b4.disabled = false;
-    b5.disabled = false;
-    b6.disabled = false;
-    b7.disabled = false;
-    b8.disabled = false;
-    b9.disabled = false;
+    for(var i=1; i<=9; i++){
+            var resetButton = document.getElementById("btn"+i);
+            resetButton.innerHTML = "";
+            resetButton.style.backgroundColor = "#5352ed";
+            resetButton.disabled = false;
+    }
 }
