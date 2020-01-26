0210cc9be703c4bbb294b1a8eef825b9c79c2f02
nangchetnamaw <nangchetnamaw@gmail.com>
2020-01-26 11:10:10 +0530

    script modified

--- Chetna_Mongmaw/calculator/assets/scripts/calc.js
+++ Chetna_Mongmaw/calculator/assets/scripts/calc.js
@@ -4,27 +4,21 @@ function display(val){
 }
 //function for computing
 function compute(){
-    let x = document.getElementById("result").value;    
+    let x = document.getElementById("result").value;
     len = x.length;
-    if (x.charAt(l-1)=="!") {
-        fact = factorial(x.slice(0, l-1));
-        document.getElementById("display").value = fact;
-        }
-
+    if(x.charAt(len-1)=="!"){
+        ans = factorial(x.slice(0, len-1));
+        document.getElementById("result").value = ans 
+    }
     else{
-    
-    x = x.replace(/sin/g, "Math.sin");
-    x = x.replace(/cos/g, "Math.cos");
-    x = x.replace(/tan/g, "Math.tan");
-    x = x.replace(/√/g, "Math.sqrt");
-    x = x.replace(/x/g, "*");
-    x = x.replace(/÷/g, "/");
-
-
-
-    let y = eval(x);
-    document.getElementById("result").value = y;
-}
+        x = x.replace(/sin/g, "Math.sin");
+        x = x.replace(/cos/g, "Math.cos");
+        x = x.replace(/tan/g, "Math.tan");
+        x = x.replace(/√/g, "Math.sqrt")
+        x = x.replace(/x/g, "*")
+        x = x.replace(/÷/g, "/")
+        let y = eval(x) 
+        document.getElementById("result").value = y }
 }
 
 //function for clearing screen 
@@ -34,11 +28,12 @@ function cscreen(){
 
 //function for clearing screen by entry
 function cescreen(){
-    text = document.getElementById("result").value;
-    lastVal = text[text.length -1];
-    document.getElementById("result").value = text.replace("lastVal","");
+    x = document.getElementById("result").value;
+    lastVal = x[x.length -1];
+    document.getElementById("result").value = x.replace("lastVal","");
 }
 
+//for factorial
 function factorial(n) { 
     var fact=1; 
     for (var i = 2; i <= n; i++) 
