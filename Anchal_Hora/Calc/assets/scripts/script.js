359b030a3446f31c2882eec37dcb8857ba0db5e9
horaanchal <horaanchal@gmail.com>
2020-01-29 11:09:12 +0530

    updated functionality of calculator

--- Anchal_Hora/Calc/assets/scripts/script.js
+++ Anchal_Hora/Calc/assets/scripts/script.js
@@ -1,29 +1,9 @@
-function insert(num){
-    document.calc.display.value= document.calc.display.value+num;
-}
-
-function fact (n){
-    var ans=1;
-    for(var i=2;i<=n;i++){
-    ans=ans*i;
-}
-    document.calc.display.value= ans;
-}
-
-
-function clearbyentry(){
-text= document.calc.display.value;
-len=text.length;
-document.calc.display.value= text.replace(text[len-1], "")
-
-}
-
-
+//function for inserting any value at screen
 function insert(input){
     document.getElementById("display").value+=input;
 }
 
-
+//function for calculating x!
 function factorial(){
     var ans=1; 
     n= document.getElementById("display").value;
@@ -33,6 +13,7 @@ function factorial(){
     document.getElementById("display").value=ans;
     }
 
+//function for sin,cos,tan,sqrt
 function operation(opr){
     val=document.getElementById("display").value;
     if(opr=='tan')
@@ -47,20 +28,19 @@ function operation(opr){
     document.getElementById("display").value=new_val;
 }
 
-
+//function for CE button
 function clearEntry(){
     txt = document.getElementById("display").value;
     l = txt.length;
     document.getElementById("display").value = txt.replace(txt[l-1] , "");
 }
 
+//function for C button
 function allClear(){
 document.getElementById("display").value="";
 }
 
-
+//function for = button
 function calc_equal(){
     document.getElementById("display").value=eval(document.getElementById("display").value);
 }
-
-
