c673e56e372a2e08894f9066f2ebe12abd6d07b4
akhtarfzn <38faizan@gmail.com>
2020-02-04 06:55:37 +0530

    login signup UI & JS, generating jwt token for employee managemnet

---
+++ Faizan_Akhtar/employeeManagement/assets/scripts/login.js
@@ -0,0 +1,29 @@
+var empLogin = {
+    email: "",
+    passwrd: "",
+};
+
+function loginSubmit(){
+    var email = document.getElementById("email").value;
+    var passwrd = document.getElementById("pwd").value;
+
+    var empLogin = {
+        email: email,
+        passwrd: passwrd,
+    };
+    console.log(empLogin);
+    api(empLogin);
+    window.location.href = "./admin.html";
+}
+
+function api(empLogin){
+    let empObject = empLogin;
+    const xhr = new XMLHttpRequest();
+    const url = "http://localhost:61496/api/Login";
+    xhr.open('POST', url);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader("Content-Type", "application/json");
+    console.log(empObject);
+    console.log(JSON.stringify(empObject));
+    xhr.send(JSON.stringify(empObject));
+}
---
+++ Faizan_Akhtar/employeeManagement/assets/scripts/signup.js
@@ -0,0 +1,39 @@
+var emp = {
+    fName: "",
+    lName: "",
+    email: "",
+    passwrd: "",
+};
+
+function signupSubmit(){
+    var fName = document.getElementById("fName").value;
+    var lName = document.getElementById("lName").value;
+    var email = document.getElementById("email").value;
+    var passwrd = document.getElementById("pwd").value;
+
+    // for (let val in emp) {
+    //     emp[val] = document.getElementById(val).value;
+    // }
+
+    var emp = {
+        fName: fName,
+        lName: lName,
+        email: email,
+        passwrd: passwrd,
+    };
+    console.log(emp);
+    api(emp);
+    window.location.href = "./admin.html";
+}
+
+function api(emp){
+    let empObject = emp;
+    const xhr = new XMLHttpRequest();
+    const url = "http://localhost:61496/api/Signup";
+    xhr.open('POST', url);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader("Content-Type", "application/json");
+    console.log(empObject);
+    console.log(JSON.stringify(empObject));
+    xhr.send(JSON.stringify(empObject));
+}
