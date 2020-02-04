4a4475c5322db228ae1f262944a5168578c7df0b
akhtarfzn <38faizan@gmail.com>
2020-02-04 13:21:52 +0530

    added jwt debugger

--- Faizan_Akhtar/employeeManagement/assets/scripts/login.js
+++ Faizan_Akhtar/employeeManagement/assets/scripts/login.js
@@ -1,7 +1,7 @@
-var empLogin = {
-    email: "",
-    passwrd: "",
-};
+// var empLogin = {
+//     email: "",
+//     passwrd: "",
+// };
 
 function loginSubmit(){
     var email = document.getElementById("uname").value;
@@ -13,17 +13,45 @@ function loginSubmit(){
     };
     console.log(empLogin);
     api(empLogin);
-    window.location.href = "./admin.html";
+    //window.location.href = "./admin.html";
 }
+
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    console.log(jsonPayload);
+    printToken(jsonPayload);
+    debugger;
+    return JSON.parse(jsonPayload);
+
+};
 
 function api(empLogin){
     let empObject = empLogin;
     const xhr = new XMLHttpRequest();
     const url = "http://localhost:61496/api/Login";
     xhr.open('POST', url);
-    xhr.responseType = 'json';
+    //xhr.responseType = 'json';
     xhr.setRequestHeader("Content-Type", "application/json");
-    console.log(empObject);
     console.log(JSON.stringify(empObject));
+    xhr.onload = () => {
+        console.log(xhr.response);
+        let jwtToken = {} ;
+        jwtToken = xhr.response; 
+        parseJwt(jwtToken);
+        debugger;
+    };
+    xhr.onerror = (err) =>{
+        console.log(err);
+    };
     xhr.send(JSON.stringify(empObject));
+    //debugger;
+}
+
+function printToken(jsonPayload){
+    console.log(jsonPayload);
 }
