fdb333a8fe8054cf2aab0be4a32306557f70fba1
Himanshu Kumar <himanshu.kumar@cybergindia.local>
2020-02-05 13:13:31 +0530

    C sharp task

---
+++ Himanshu_Kumar/UsersTask/Controllers/LoginController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace UsersTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        public IConfiguration _config;
+        DbOps.IDbOps _dbOps;
+        public LoginController(DbOps.IDbOps dbOps, IConfiguration config)
+        {
+            _dbOps = dbOps;
+            _config = config;
+        }
+
+        [HttpPost]
+        public IActionResult logIn([FromBody] CustomModels.customUsers customUser)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(customUser);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+            return response;
+        }
+
+        private string GenerateJSONWebToken(CustomModels.customUsers loginInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, _config["Jwt:Issuer"]),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                new Claim("Namee", loginInfo.Namee),
+                new Claim("Email", loginInfo.Email),
+                new Claim("JobRole", loginInfo.Jobrole)
+            };
+
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              claims,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private CustomModels.customUsers AuthenticateUser(CustomModels.customUsers usersDetails)
+        {
+            CustomModels.customUsers user = null;
+            user = _dbOps.checkUser(usersDetails);
+            return user;
+        }
+    }
+}
---
+++ Himanshu_Kumar/UsersTask/DbModels/csharpTaskContext.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace UsersTask.DbModels
+{
+    public partial class csharpTaskContext : DbContext
+    {
+        public csharpTaskContext()
+        {
+        }
+
+        public csharpTaskContext(DbContextOptions<csharpTaskContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Userdetails> Userdetails { get; set; }
+        public IEnumerable<object> UserDetails { get; internal set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("server=.;database=csharpTask; trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Userdetails>(entity =>
+            {
+                entity.ToTable("userdetails");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Age).HasColumnName("age");
+
+                entity.Property(e => e.Email)
+                    .HasColumnName("email")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .HasColumnName("gender")
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Jobrole)
+                    .HasColumnName("jobrole")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Namee)
+                    .HasColumnName("namee")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Pass)
+                    .HasColumnName("pass")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
