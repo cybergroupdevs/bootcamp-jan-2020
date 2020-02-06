24e036d7342ac6d0e313d9d608bd375a7e6d994a
akhtarfzn <38faizan@gmail.com>
2020-02-06 16:01:13 +0530

    added DbModels and CustomModels file

---
+++ Faizan_Akhtar/employeeManagement/api/EmployeeDBContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EmployeeManagement.DbModels
+{
+    public partial class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext()
+        {
+        }
+
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TEmployee> TEmployee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=CYG290\\SQLEXPRESS;Database=EmployeeDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TEmployee>(entity =>
+            {
+                entity.HasKey(e => e.EmpId);
+
+                entity.ToTable("tEmployee");
+
+                entity.Property(e => e.EmpId).HasColumnName("EmpID");
+
+                entity.Property(e => e.Designation)
+                    .HasColumnName("designation")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .HasColumnName("email")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FName)
+                    .HasColumnName("fName")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Lname)
+                    .HasColumnName("LName")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Manager)
+                    .HasColumnName("manager")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Passwrd)
+                    .HasColumnName("passwrd")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Project)
+                    .HasColumnName("project")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary).HasColumnName("salary");
+            });
+        }
+    }
+}
