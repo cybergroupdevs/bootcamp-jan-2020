f86fc9c7e9e27c6162d10746f0906a69ae5c516a
Himanshu Kumar <himanshu.kumar@cybergindia.local>
2020-02-11 10:34:11 +0530

    CRUD basics with Node

---
+++ Himanshu_Kumar/nodeProject/controllers/index.js
@@ -0,0 +1,7 @@
+//index.js is an entry point for controllers or any other folder
+const employees = require('./employee');
+
+
+module.exports={
+    employees:employees
+}
---
+++ Himanshu_Kumar/nodeProject/database/config.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+
+const url="mongodb://localhost:27017/bootcamp"
+
+
+mongoose.Promise=global.Promise;
+
+mongoose.connect(url,{useNewUrlParser:true,keepAlive:1}).then((res)=>{
+    console.log("Established connection  with database");
+    //console.log(res);
+}).catch(error=>{
+    console.log(error.message);
+})
+module.exports=mongoose;
