dfccf9bb9093b58b6a2b2837d8444c9219439eeb
balani98 <balanideepanshu92@gmail.com>
2020-02-12 11:15:59 +0530

    connection string removed

--- Deepanshu_Balani/dotnetproject/api/apiProject_bootcamp/DbModels/apiproject2Context.cs
+++ Deepanshu_Balani/dotnetproject/api/apiProject_bootcamp/DbModels/apiproject2Context.cs
@@ -17,14 +17,7 @@ namespace api_new2.DbModels
 
         public virtual DbSet<Company> Company { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("server=CYG363; database=apiproject2; Trusted_Connection=True;");
-            }
-        }
+        
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
