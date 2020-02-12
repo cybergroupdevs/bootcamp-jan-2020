679e55cbdac6fbedd5addff6ad456fe2e0451a1a
Shivani Bansal <bansalshivani703@gmail.com>
2020-02-12 11:43:02 +0530

    added view

---
+++ Shivani_Bansal/NodeAssignment/view/assets/scripts/dashboard.js
@@ -0,0 +1,46 @@
+
+function sendJSON()
+{
+    fetch('https://localhost:44371/api/signup')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    return data;
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+function appendData(data)
+{
+    var users = document.getElementById("users");
+    console.log(users);
+    console.log(data.length);
+  
+    for (var i = 0; i < data.length; i++) {
+      var object = data[i];
+      var arrayOfKeys = Object.keys(object)
+      console.log(arrayOfKeys)
+
+      var tr = document.createElement("TR");
+      tr.setAttribute("id", `tr${i}`);
+      var k = 0;
+      for (var j in data[i]){
+       
+        var td = document.createElement("TD");
+        td.innerHTML = data[i].arrayOfKeys[k];
+        console.log(document.getElementById(`tr${i}`));
+
+        document.getElementById(`tr${i}`).appendChild(td);
+        k = k + 1;
+      }
+      document.getElementById("myTable").appendChild(tr);
+        
+        
+    }
+  }
+  
+}
---
+++ Shivani_Bansal/NodeAssignment/view/assets/scripts/login.js
@@ -0,0 +1,43 @@
+function sendJSON(){
+
+    let email = document.getElementById("exampleInputEmail1").value;
+    let password = document.getElementById("exampleInputPassword1").value;
+
+    //creating xhr request
+    var xhr = new XMLHttpRequest();
+    
+    let url  = "http://localhost:5000/login";
+
+    //open a connection 
+    xhr.open("POST", url, true);
+    
+    //set the request header i.e which type of content is being sent
+    xhr.setRequestHeader("Content-Type", "application/json");
+    
+    //create a state change callback
+    xhr.onreadystatechange = function () 
+    {
+        mytoken = this.responseText;
+        
+        // console.log(mytoken);
+        
+        if (XMLHttpRequest.DONE && xhr.status ===200)
+        {
+            window.open("./dashboard.html");
+           
+        }
+        else
+        {
+            alert("not registered user");
+        }
+    
+    };
+    
+    let obj = {"email": email, "password": password};
+    
+    var data = JSON.stringify(obj)
+
+    // console.log(data);
+    xhr.send(data);
+    event.preventDefault();
+}
